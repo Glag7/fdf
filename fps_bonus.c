fe88b8af9c165343b3150ca8312bbf03fc148829
Glag7 <glaguyonie@gmail.com>
2024-02-29 20:17:05 +0100

    accurate fps counter

--- fps_bonus.c
+++ fps_bonus.c
@@ -6,7 +6,7 @@
 /*   By: glaguyon <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/26 17:25:41 by glaguyon          #+#    #+#             */
-/*   Updated: 2024/02/29 19:54:03 by glag             ###   ########.fr       */
+/*   Updated: 2024/02/29 20:15:02 by glag             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@
 #define GRE 0xFF00FF00
 
 //space because mlx
-static inline void	drawfps(t_mlx *mlx, int fps_val, char draw)
+static inline void	drawfps(t_mlx *mlx, char draw)
 {
 	static char	num[8] = "fps:    ";
 	static int	todraw = 0;
@@ -45,9 +45,9 @@ static inline void	drawfps(t_mlx *mlx, int fps_val, char draw)
 			mlx_string_put(mlx->mlx, mlx->win, 0, 10, RED, num);
 			num[5] = ' ';
 		}
+		todraw = 0;
 	}
-	else
-		todraw = fps_val;
+	todraw++;
 }
 
 int	putfps(void *mlx_)
@@ -59,12 +59,12 @@ int	putfps(void *mlx_)
 
 	mlx = (t_mlx *)mlx_;
 	clock_gettime(CLOCK_MONOTONIC_RAW, &curr);
-	fps_val = 1.0 / ((float)(curr.tv_sec - past.tv_sec)
+	fps_val = 1.0f / ((float)(curr.tv_sec - past.tv_sec)
 			+ (float)(curr.tv_nsec - past.tv_nsec) *1e-9);
 	fps(1, fps_val);
-	drawfps(mlx, round(fps_val), 0);
+	drawfps(mlx, 0);
 	if (curr.tv_sec > past.tv_sec)
-		drawfps(mlx, 0, 1);
+		drawfps(mlx, 1);
 	printf("fps: %f\n", fps_val);
 	past = curr;
 	return (0);
